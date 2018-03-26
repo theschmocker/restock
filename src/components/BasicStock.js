e24ce90e5b8c90e5857daceaf94b18f187a921ea
theschmocker <jacobcs1996@gmail.com>
2018-03-26 17:10:02 -0500

    wrap BasicStock in withStockInfo higher-order component

--- src/components/BasicStock.js
+++ src/components/BasicStock.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
+import withStockInfo from '../containers/withStockInfo';
 
 const BasicStock = ({ stock }) => (
     <StockWrapper>
-        <h3>{stock.name}</h3>
+        <h3>{stock.companyName}</h3>
         <p>{stock.symbol}</p>
         <h2>{stock.latestPrice}</h2>
     </StockWrapper>
 )
 
-export default BasicStock;
+export default withStockInfo(BasicStock);
 
 
 // Styled Components
